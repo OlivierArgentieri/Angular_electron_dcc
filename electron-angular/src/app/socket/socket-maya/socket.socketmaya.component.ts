36d0ccb7e189dab82463fbd7fb17c1f978f12c5a
OlivierArgentieri <olivierargentieri@gmail.com>
2020-12-17 15:17:00 +0100

    refactor socket-maya component

--- electron-angular/src/app/socket/socket-maya/socket.socketmaya.component.ts
+++ electron-angular/src/app/socket/socket-maya/socket.socketmaya.component.ts
@@ -1,15 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { SocketConfig } from '../models/socket-config';
-
-import { io } from 'socket.io-client';
-import { Router, ActivatedRoute } from '@angular/router';
-
-// SocketIoModule
-//import { Socket } from 'ngx-socket-io';
-//import { Injectable } from '@angular/core';
-
-//
-//const socket = io("ws://localhost:1234/");
+import { ActivatedRoute } from '@angular/router';
+import { MayaService } from '../services/maya/maya-service';
+import * as litegraph from 'litegraph.js'
 
 @Component({
   selector: 'app-socketmaya',
@@ -17,44 +9,27 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./socket.socketmaya.component.scss']
 })
 export class SocketMayaComponent implements OnInit {
-
   port: number = 0;
-  host: string = "";
+  
   message: string = "";
 
-  socket = null;
+  private route:ActivatedRoute = null;
+  private service:MayaService = null;
 
-  constructor(private route: ActivatedRoute,) {
+  constructor(private _route: ActivatedRoute,private _service: MayaService) {
+    this.route = _route;
+    this.service = _service;
   }
 
-  setupSocketConnection() {
-    this.socket = io('http://localhost:3000', { autoConnect: false, transports: ['websocket'], upgrade: false });
-    this.socket.open();
-  }
-
-  setupAction() {
-    if(this.socket == null) return;
-
-    this.socket.on("callback", (message)=>{
-      console.log(message)
-    });
-  }
-
- 
 
   ngOnInit(): void {
-    this.setupSocketConnection();
-    this.setupAction();
-
     this.route.queryParams.subscribe(params => {
       this.port = params['port'];
     });
   }
 
-  Emit() {
-    // if(this.socket != null) return;
-    //this.setupSocketConnection();
-    this.socket.emit("mayaCommand", this.message);
+  sendCommand() {
+    this.service.sendCommand(this.message);
   }
 
   
