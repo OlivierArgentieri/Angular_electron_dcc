75a86beb154451e9dfae776999b79a0d4dc814f7
OlivierArgentieri <olivierargentieri@gmail.com>
2021-01-09 12:15:24 +0100

    update handler

--- pipeline2/engines/houdini_handler.py
+++ pipeline2/engines/houdini_handler.py
@@ -6,7 +6,5 @@ for key, value in tmp_modules.iteritems():
     if key.startswith('pipeline2.'):
         sys.modules.pop(key, None)
 
-from pipeline2.engines.actions.houdini import *
-from pipeline2.engines.servers.houdini import HoudiniSocketServer 
-
+from pipeline2.engines.servers.houdini import HoudiniSocketServer
 server = HoudiniSocketServer()
--- pipeline2/engines/maya_handler.py
+++ pipeline2/engines/maya_handler.py
@@ -5,8 +5,8 @@ tmp_modules = sys.modules.copy()
 for key, value in tmp_modules.iteritems():
     if key.startswith('pipeline2.'):
         sys.modules.pop(key, None)
-
-from pipeline2.engines.actions.maya import *
-from pipeline2.engines.servers.maya import MayaSocketServer 
-
+        
+from pipeline2.engines.servers.maya import MayaSocketServer
 server = MayaSocketServer()
+
+
