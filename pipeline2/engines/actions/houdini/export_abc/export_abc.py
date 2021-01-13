8c17fbdeaeeca5e02f6e31692a56f3e58346b6ff
OlivierArgentieri <olivierargentieri@gmail.com>
2021-01-13 09:02:01 +0100

    Update export_abc.py

--- pipeline2/engines/actions/houdini/export_abc/export_abc.py
+++ pipeline2/engines/actions/houdini/export_abc/export_abc.py
@@ -1,14 +1,2 @@
-from pipeline2.engines.actions.common.export_abc import ExportAbc
-
-
 def run(*args, **kwargs):
     print("aaaaaaaa")
-
-
-class HoudiniExportAbc(ExportAbc):
-    @staticmethod
-    def run(*args, **kwargs):
-
-        print('test')
-        return
-        # from parameters
