4292eaae254995d7ee4380e3a2b58d6ba06d728c
OlivierArgentieri <olivierargentieri@gmail.com>
2021-01-21 13:46:33 +0100

    Update install.sh

--- plugins/install.sh
+++ plugins/install.sh
@@ -3,5 +3,6 @@
 # --- CP TO HOUDINI DIRECTORY ---
 # path structure : ~/houdiniX.X/
 read -p "Enter version of hodini (example: 18.0):" _houdiniVersion
-_cpto=~"/houdini$_houdiniVersion"
-echo $_cpto
+#_cpto=~"/houdini$_houdiniVersion"
+_cpto=~/$_houdiVersion
+cp -R ./houdini $_cpto
